34b5315f525c9523073031d5eefb0e8f67510944
Katie Leong <112890169+Kuonin@users.noreply.github.com>
2025-08-17 17:23:09 -0700

    changed the way post gets data

--- cgi-bin/py-post-echo.py
+++ cgi-bin/py-post-echo.py
@@ -3,8 +3,9 @@ print("Cache-Control: no-cache")
 print("Content-Type: text/html\r\n\r\n")
 
 import sys
-import cgi
 import html
+import os
+import urllib.parse as ulp
 
 # print HTML file top
 print('''<!DOCTYPE html>
@@ -13,8 +14,19 @@ print('''<!DOCTYPE html>
 <hr>''')
 
 # Read in the Post data
-form = cgi.FieldStorage()
-form_data = form.getvalue('data', '')
+# Get the current URL which includes the Query String
+url = os.environ["REQUEST_URI"]
+
+# Parse the URL into its subcomponents
+parsed = ulp.urlparse(url)
+
+# Parse the Query String portion of the URL;
+# this returns a dictionary of 'key:value' pairs,
+# with 'key' being the name of the input in the HTML form,
+# and 'value' being a list which contains what was
+# entered by the user as that input in the same HTML form
+dic = ulp.parse_qs(parsed.query, keep_blank_values=True)
+
 
 # Print out the Message Body
 print("<b>Message Body:</b><br />\n")
@@ -22,10 +34,10 @@ print("<ul>\n")
 
 # Print out the Query string
 loop = 0
-for key in form.keys():
+for key in dic.keys():
     loop += 1
     if loop % 2 != 0:
-        value = html.escape(form.getvalue(key, ''))
+        value = html.escape(dic.getvalue(key, ''))
         print(f"<li>{html.escape(key)} = {value}</li>\n")
 
 print("</ul>\n")
