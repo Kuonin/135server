fa17c571c1b51e6e406f2fac96c707b6eab57045
Katie Leong <112890169+Kuonin@users.noreply.github.com>
2025-08-28 12:33:31 -0700

    added static data collection to collector

--- collector.js
+++ collector.js
@@ -1,5 +1,29 @@
 
-let user = window.navigator.userAgent;
+let nav = window.navigator;
+let user = nav.userAgent;
+let language = nav.language;
+let allowCookie = nav.cookieEnabled;
+
+let s = window.screen;
+let sWidth = s.width;
+let sHeight = s.height;
+
+let wWidth = window.innerWidth;
+let wHeight = window.innerHeight;
+
+//js checked using no script tag in index.html
+let js = "enabled";
+let image = "unavailable";
+
+const img = new Image();
+img.onload = function(){
+    image = "available";
+}
+
+let css = "disabled";
+if(document.querySelector("footer").getPropertyValue('position') === 'fixed'){
+    css = "enabled";
+}
 
 async function sendData(){
     const response = await fetch("https://katiel.site/json/posts", {
@@ -7,7 +31,15 @@ async function sendData(){
     headers: {
         "Content-Type": "application/json",
     },
-    body: JSON.stringify({ userAgent: user }),
+    body: JSON.stringify({
+         userAgent: user,
+         userLan: language,
+         cookieEnabled: allowCookie,
+         screenWidth: sWidth,
+         screenHeight: sHeight,
+         windowWidth: wWidth,
+         windowHeight: wHeight
+         }),
     // …
     });
 }
