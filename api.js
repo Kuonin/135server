8670f263a8f2d324498e4531a6942cac6d304265
Katie Leong <112890169+Kuonin@users.noreply.github.com>
2025-08-31 23:14:33 -0700

    testing to see if the different methods work on /static route

--- api.js
+++ api.js
@@ -12,16 +12,52 @@ const stat = JSON.stringify([
 
 
 const requestListener = function (req, res) {
-    res.setHeader("Content-Type", "application/json");
-    switch(req.url){
-        case "/static":
-            res.writeHead(200);
-            res.end(stat);
-            break
-        default:
-            res.writeHead(404);
-            res.end(JSON.stringify({error: "Resource not found :("}));
+    try{
+        res.setHeader("Content-Type", "application/json");
+        const methodType = req.method.toUpperCase();
+        switch(req.url){
+            case "/static":
+                switch(methodType){
+                    case 'GET':
+                        res.writeHead(200);
+                        res.end(`We received ${methodType} type request`);
+                        break;
+                    case 'POST':
+                        res.writeHead(200);
+                        res.end(`We received ${methodType} type request`);
+                        break;
+                    case 'PUT':
+                        res.writeHead(200);
+                        res.end(`We received ${methodType} type request`);
+                        break;
+                    case 'DELETE':
+                        res.writeHead(200);
+                        res.end(`We received ${methodType} type request`);
+                        break;
+                    default:
+                        res.writeHead(404);
+                        res.end(JSON.stringify({error: "Method not found :("}));
+                }
+                break
+            default:
+                res.writeHead(404);
+                res.end(JSON.stringify({error: "Resource not found :("}));
+        }
+        
+
+    }catch (error){
+        res.writeHead(400);
+        res.end(error.message);
     }
+    // switch(req.url){
+    //     case "/static":
+    //         res.writeHead(200);
+    //         res.end(stat);
+    //         break
+    //     default:
+    //         res.writeHead(404);
+    //         res.end(JSON.stringify({error: "Resource not found :("}));
+    // }
     // res.writeHead(200);
     // res.end(`{"message": "This is a JSON response"}`);
 };
