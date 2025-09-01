5855dd9fded58e6e188d8ba008d6b3f920544653
Katie Leong <112890169+Kuonin@users.noreply.github.com>
2025-09-01 16:57:27 -0700

    testing basic creation following a guide

--- api.js
+++ api.js
@@ -12,6 +12,39 @@ async function main() {
  
         // Make the appropriate DB calls
         await  listDatabases(client);
+        await createListing(client,
+        {
+            name: "Lovely Loft",
+            summary: "A charming loft in Paris",
+            bedrooms: 1,
+            bathrooms: 1
+        }
+    );
+    // Create 3 new listings
+        await createMultipleListings(client, [
+            {
+                name: "Infinite Views",
+                summary: "Modern home with infinite views from the infinity pool",
+                property_type: "House",
+                bedrooms: 5,
+                bathrooms: 4.5,
+                beds: 5
+            },
+            {
+                name: "Private room in London",
+                property_type: "Apartment",
+                bedrooms: 1,
+                bathroom: 1
+            },
+            {
+                name: "Beautiful Beach House",
+                summary: "Enjoy relaxed beach living in this house with a private beach",
+                bedrooms: 4,
+                bathrooms: 2.5,
+                beds: 7,
+                last_review: new Date()
+            }
+        ]);
  
     } catch (e) {
         console.error(e);
@@ -25,33 +58,22 @@ async function listDatabases(client){
     console.log("Databases:");
     databasesList.databases.forEach(db => console.log(` - ${db.name}`));
 };
+async function createListing(client, newListing){
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews").insertOne(newListing);
+    console.log(`New listing created with the following id: ${result.insertedId}`);
+}
+async function createMultipleListings(client, newListings){
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews").insertMany(newListings);
+
+    console.log(`${result.insertedCount} new listing(s) created with the following id(s):`);
+    console.log(result.insertedIds);       
+}
+
+
 main().catch(console.error);
 
 
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const bodyParser = require('body-parser');
-
-// const app = express();
-// const port = 5000;
-
-// const userRoutes = require('./routes/user');
-// app.use('/users', userRoutes);
-
-// // Middleware
-// app.use(bodyParser.json());
-
-// // MongoDB connection
-// mongoose.connect(conString, { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(() => console.log('MongoDB connected...'))
-//     .catch(err => console.log(err));
-
-// // Start the server
-// app.listen(port, () => {
-//     console.log(`Server running on port ${port}`);
-// });
-
-
+//------------------------------------Part 0 code
 
 // const http = require("http");
 // const host = 'localhost';
