4de2de79045233791e9e57470d47513fc674d81b
Katie Leong <112890169+Kuonin@users.noreply.github.com>
2025-08-31 23:01:49 -0700

    testing /static endpoint

--- api.js
+++ api.js
@@ -3,10 +3,27 @@ const http = require("http");
 const host = 'localhost';
 const port = 5000;
 
+//Test Data for the routes
+
+const stat = JSON.stringify([
+    {sessionid: 1, data: "hello world"},
+    {sessionid: 2, data: "goodbye world"}
+]);
+
+
 const requestListener = function (req, res) {
     res.setHeader("Content-Type", "application/json");
-    res.writeHead(200);
-    res.end(`{"message": "This is a JSON response"}`);
+    switch(req.url){
+        case "/static":
+            res.writeHead(200);
+            res.end(stat);
+            break
+        default:
+            res.writeHead(404);
+            res.end(JSON.stringify({error: "Resource not found :("}));
+    }
+    // res.writeHead(200);
+    // res.end(`{"message": "This is a JSON response"}`);
 };
 
 const server = http.createServer(requestListener);
