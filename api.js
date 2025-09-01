e92f42cec9d59f24dca3d841514c5c95b5aadd0d
Katie Leong <112890169+Kuonin@users.noreply.github.com>
2025-09-01 15:25:55 -0700

    trying to use mangodb

--- api.js
+++ api.js
@@ -1,98 +1,114 @@
-const http = require("http");
 
-const host = 'localhost';
+const conString = "mongodb+srv://kyleong_db_user:PKldRtHu66E76w1H@cluster0.agfodrd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
 const port = 5000;
 
-//Test Data for the routes
+const userRoutes = require('./routes/user');
+app.use('/users', userRoutes);
 
-let data = [
-    {"sessionid": 1, "data": "hello world"},
-    {"sessionid": 2, "data": "goodbye world"}
-];
+// Middleware
+app.use(bodyParser.json());
 
-let findData = (id) => {
-    return data.find((session) => {
-        if(session.sessionid == id)
-            return session;
-    });
-}
+// MongoDB connection
+mongoose.connect(conString, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err));
 
-let stat = JSON.stringify(data);
+// Start the server
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
 
 
-const requestListener = function (req, res) {
-    try{
-        res.setHeader("Content-Type", "application/json");
-        const methodType = req.method.toUpperCase();
-        const url = req.url;
-        let parts = url.split("/");
-        switch(parts[1]){ // should be parts = [ '', 'static', 'id']
-            case "static":
-                switch(methodType){
-                    case 'GET':
-                        if(parts.length == 2){ //send all the data
-                            res.writeHead(200);
-                            res.end(stat);
-                        }
-                        else if(parts.length == 3){
-                            getMethodHandler(parts[2], req, res);
-                        }
-                        else{
-                            res.writeHead(404);
-                            res.end(JSON.stringify({error: "Url not found :("}));
-                        }
-                        break;
-                    case 'POST':
-                        res.writeHead(200);
-                        res.end(`We received ${methodType} type request`);
-                        break;
-                    case 'PUT':
-                        res.writeHead(200);
-                        res.end(`We received ${methodType} type request`);
-                        break;
-                    case 'DELETE':
-                        res.writeHead(200);
-                        res.end(`We received ${methodType} type request`);
-                        break;
-                    default:
-                        res.writeHead(404);
-                        res.end(JSON.stringify({error: "Method not found :("}));
-                }
-                break
-            default:
-                res.writeHead(404);
-                res.end(JSON.stringify({error: "Resource not found :("}));
-        }
+
+// const http = require("http");
+// const host = 'localhost';
+// const port = 5000;
+
+// //Test Data for the routes
+
+// let data = [
+//     {"sessionid": 1, "data": "hello world"},
+//     {"sessionid": 2, "data": "goodbye world"}
+// ];
+
+// let findData = (id) => {
+//     return data.find((session) => {
+//         if(session.sessionid == id)
+//             return session;
+//     });
+// }
+
+// let stat = JSON.stringify(data);
+
+
+// const requestListener = function (req, res) {
+//     try{
+//         res.setHeader("Content-Type", "application/json");
+//         const methodType = req.method.toUpperCase();
+//         const url = req.url;
+//         let parts = url.split("/");
+//         switch(parts[1]){ // should be parts = [ '', 'static', 'id']
+//             case "static":
+//                 switch(methodType){
+//                     case 'GET':
+//                         if(parts.length == 2){ //send all the data
+//                             res.writeHead(200);
+//                             res.end(stat);
+//                         }
+//                         else if(parts.length == 3){
+//                             getMethodHandler(parts[2], req, res);
+//                         }
+//                         else{
+//                             res.writeHead(404);
+//                             res.end(JSON.stringify({error: "Url not found :("}));
+//                         }
+//                         break;
+//                     case 'POST':
+//                         res.writeHead(200);
+//                         res.end(`We received ${methodType} type request`);
+//                         break;
+//                     case 'PUT':
+//                         res.writeHead(200);
+//                         res.end(`We received ${methodType} type request`);
+//                         break;
+//                     case 'DELETE':
+//                         res.writeHead(200);
+//                         res.end(`We received ${methodType} type request`);
+//                         break;
+//                     default:
+//                         res.writeHead(404);
+//                         res.end(JSON.stringify({error: "Method not found :("}));
+//                 }
+//                 break
+//             default:
+//                 res.writeHead(404);
+//                 res.end(JSON.stringify({error: "Resource not found :("}));
+//         }
         
 
-    }catch (error){
-        res.writeHead(400);
-        res.end(error.message);
-    }
-    // switch(req.url){
-    //     case "/static":
-    //         res.writeHead(200);
-    //         res.end(stat);
-    //         break
-    //     default:
-    //         res.writeHead(404);
-    //         res.end(JSON.stringify({error: "Resource not found :("}));
-    // }
-    // res.writeHead(200);
-    // res.end(`{"message": "This is a JSON response"}`);
-};
+//     }catch (error){
+//         res.writeHead(400);
+//         res.end(error.message);
+//     }
+// };
 
-const getMethodHandler = (id, req, res) => {
-    let session = findData(id);
-    if(!session){
-        res.writeHead(400);
-        res.end(`The session with id ${id} is not found.`);
-    }
-    res.writeHead(200);
-    res.end(JSON.stringify(session));
-}
+// const getMethodHandler = (id, req, res) => {
+//     let session = findData(id);
+//     if(!session){
+//         res.writeHead(400);
+//         res.end(`The session with id ${id} is not found.`);
+//     }
+//     res.writeHead(200);
+//     res.end(JSON.stringify(session));
+// }
 
-const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
-});
+// const server = http.createServer(requestListener);
+// server.listen(port, host, () => {
+//     console.log(`Server is running on http://${host}:${port}`);
+// });
