52227825513eb539622a5c427d6b240fcf05f3f7
Katie Leong <112890169+Kuonin@users.noreply.github.com>
2025-09-02 16:04:20 -0700

    finished route creation

--- api.js
+++ api.js
@@ -1,190 +1,6 @@
 
 const conString = "mongodb+srv://kyleong_db_user:PKldRtHu66E76w1H@cluster0.agfodrd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-/*
-async function main() {
-	// we'll add code here soon
-    const {MongoClient} = require('mongodb');
-    const client = new MongoClient(conString);
 
-    try {
-        // Connect to the MongoDB cluster
-        await client.connect();
- 
-        // Make the appropriate DB calls
-        await  listDatabases(client);
-
-        await deleteListingByName(client, "Cozy Cottage");
-
-        await deleteListingsScrapedBeforeDate(client, new Date("2019-02-15"));
-
-        // await updateListingByName(client, "Infinite Views", { bedrooms: 6, beds: 8 });
-
-        // await upsertListingByName(client, "Cozy Cottage", { name: "Cozy Cottage", bedrooms: 2, bathrooms: 1 });
-
-        // await upsertListingByName(client, "Cozy Cottage", { beds: 2 });
-
-        // await updateAllListingsToHavePropertyType(client);
-
-        // Find the listing named "Infinite Views" that we created in create.js
-        // await findOneListingByName(client, "Infinite Views");
-
-        // Find up to 5 listings with at least 4 bedrooms and at least 2 bathrooms
-        // If you recently ran create.js, a listing named Beautiful Beach House should be included in the results 
-        // await findListingsWithMinimumBedroomsBathroomsAndMostRecentReviews(client, {
-        //     minimumNumberOfBedrooms: 4,
-        //     minimumNumberOfBathrooms: 2,
-        //     maximumNumberOfResults: 5
-        // });
-    //     await createListing(client,
-    //     {
-    //         name: "Lovely Loft",
-    //         summary: "A charming loft in Paris",
-    //         bedrooms: 1,
-    //         bathrooms: 1
-    //     }
-    // );
-    // Create 3 new listings
-        // await createMultipleListings(client, [
-        //     {
-        //         name: "Infinite Views",
-        //         summary: "Modern home with infinite views from the infinity pool",
-        //         property_type: "House",
-        //         bedrooms: 5,
-        //         bathrooms: 4.5,
-        //         beds: 5
-        //     },
-        //     {
-        //         name: "Private room in London",
-        //         property_type: "Apartment",
-        //         bedrooms: 1,
-        //         bathroom: 1
-        //     },
-        //     {
-        //         name: "Beautiful Beach House",
-        //         summary: "Enjoy relaxed beach living in this house with a private beach",
-        //         bedrooms: 4,
-        //         bathrooms: 2.5,
-        //         beds: 7,
-        //         last_review: new Date()
-        //     }
-        // ]);
- 
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
-};
-async function listDatabases(client){
-    databasesList = await client.db().admin().listDatabases();
- 
-    console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
-async function createListing(client, newListing){
-    const result = await client.db("sample_airbnb").collection("listingsAndReviews").insertOne(newListing);
-    console.log(`New listing created with the following id: ${result.insertedId}`);
-}
-async function createMultipleListings(client, newListings){
-    const result = await client.db("sample_airbnb").collection("listingsAndReviews").insertMany(newListings);
-
-    console.log(`${result.insertedCount} new listing(s) created with the following id(s):`);
-    console.log(result.insertedIds);       
-}
-
-async function findOneListingByName(client, nameOfListing) {
-    const result = await client.db("sample_airbnb").collection("listingsAndReviews").findOne({ name: nameOfListing });
-
-    if (result) {
-        console.log(`Found a listing in the collection with the name '${nameOfListing}':`);
-        console.log(result);
-    } else {
-        console.log(`No listings found with the name '${nameOfListing}'`);
-    }
-}
-async function findListingsWithMinimumBedroomsBathroomsAndMostRecentReviews(client, {
-    minimumNumberOfBedrooms = 0,
-    minimumNumberOfBathrooms = 0,
-    maximumNumberOfResults = Number.MAX_SAFE_INTEGER
-} = {}) {
-
-    // See https://mongodb.github.io/node-mongodb-native/3.6/api/Collection.html#find for the find() docs
-    const cursor = client.db("sample_airbnb").collection("listingsAndReviews")
-        .find({
-            bedrooms: { $gte: minimumNumberOfBedrooms },
-            bathrooms: { $gte: minimumNumberOfBathrooms }
-        }
-        )
-        .sort({ last_review: -1 })
-        .limit(maximumNumberOfResults);
-
-    // Store the results in an array
-    const results = await cursor.toArray();
-
-    // Print the results
-    if (results.length > 0) {
-        console.log(`Found listing(s) with at least ${minimumNumberOfBedrooms} bedrooms and ${minimumNumberOfBathrooms} bathrooms:`);
-        results.forEach((result, i) => {
-            const date = new Date(result.last_review).toDateString();
-
-            console.log();
-            console.log(`${i + 1}. name: ${result.name}`);
-            console.log(`   _id: ${result._id}`);
-            console.log(`   bedrooms: ${result.bedrooms}`);
-            console.log(`   bathrooms: ${result.bathrooms}`);
-            console.log(`   most recent review date: ${date}`);
-        });
-    } else {
-        console.log(`No listings found with at least ${minimumNumberOfBedrooms} bedrooms and ${minimumNumberOfBathrooms} bathrooms`);
-    }
-}
-
-async function updateListingByName(client, nameOfListing, updatedListing) {
-    const result = await client.db("sample_airbnb").collection("listingsAndReviews")
-                        .updateOne({ name: nameOfListing }, { $set: updatedListing });
-
-    console.log(`${result.matchedCount} document(s) matched the query criteria.`);
-    console.log(`${result.modifiedCount} document(s) was/were updated.`);
-}
-
-async function upsertListingByName(client, nameOfListing, updatedListing) {
-    const result = await client.db("sample_airbnb").collection("listingsAndReviews")
-                        .updateOne({ name: nameOfListing }, 
-                                   { $set: updatedListing }, 
-                                   { upsert: true });
-    console.log(`${result.matchedCount} document(s) matched the query criteria.`);
-
-    if (result.upsertedCount > 0) {
-        console.log(`One document was inserted with the id ${result.upsertedId._id}`);
-    } else {
-        console.log(`${result.modifiedCount} document(s) was/were updated.`);
-    }
-}
-
-async function updateAllListingsToHavePropertyType(client) {
-    const result = await client.db("sample_airbnb").collection("listingsAndReviews")
-                        .updateMany({ property_type: { $exists: false } }, 
-                                    { $set: { property_type: "Unknown" } });
-    console.log(`${result.matchedCount} document(s) matched the query criteria.`);
-    console.log(`${result.modifiedCount} document(s) was/were updated.`);
-}
-
-async function deleteListingByName(client, nameOfListing) {
-    const result = await client.db("sample_airbnb").collection("listingsAndReviews")
-            .deleteOne({ name: nameOfListing });
-    console.log(`${result.deletedCount} document(s) was/were deleted.`);
-}
-
-async function deleteListingsScrapedBeforeDate(client, date) {
-    const result = await client.db("sample_airbnb").collection("listingsAndReviews")
-        .deleteMany({ "last_scraped": { $lt: date } });
-    console.log(`${result.deletedCount} document(s) was/were deleted.`);
-}
-
-
-
-main().catch(console.error);
-*/
 
 //------------------------------------Part 0 code + part 1
 
@@ -197,7 +13,7 @@ const port = 5000;
 
 //Test Data for the routes
 
-let database = "test_data";
+// let database = "test_data";
 
 let data = [
     {"sessionid": 1, "data": "hello world"},
@@ -213,29 +29,6 @@ let findData = (id) => {
 
 let stat = JSON.stringify(data);
 
-// async function main(data, method) {
-
-//     const {MongoClient} = require('mongodb');
-//     const client = new MongoClient(conString);
-
-//     try {
-//         // Connect to the MongoDB cluster
-//         await client.connect();
-//         switch(method){
-//             case "POST":
-//                 const result = await client.db(database).collection("testing").insertOne(data);
-//                 console.log(`New listing created with the following id: ${result.insertedId}`);
-//                 break
-//         }
-        
- 
-//     } catch (e) {
-//         console.error(e);
-//     } finally {
-//         await client.close();
-//     }
-// };
-
 const requestListener = function (req, res) {
     try{
         res.setHeader("Content-Type", "application/json");
@@ -243,54 +36,62 @@ const requestListener = function (req, res) {
         const methodType = req.method.toUpperCase();
         const url = req.url;
         let parts = url.split("/");
+        let database = "";
         switch(parts[1]){ // should be parts = [ '', 'static', 'id']
             case "static":
-                switch(methodType){
-                    case 'GET':
-                        if(parts.length == 2){ //send all the data
-                            getMethodHandler(null, req, res);
-                        }
-                        else if(parts.length == 3){
-                            getMethodHandler(parts[2], req, res);
-                        }
-                        else{
-                            res.writeHead(404);
-                            res.end(JSON.stringify({error: "Url not found :("}));
-                        }
-                        break;
-                    case 'POST':
-                        getRequestBodyAndGenerateResponse(req, res,po);
-                        break;
-                    case 'PUT':
-                        // res.writeHead(200);
-                        // res.end(`We received ${methodType} type request`);
-                        if(parts.length == 3){
-                            putMethodHandler(parts[2],req, res);
-                        }
-                        else{
-                            res.writeHead(404);
-                            res.end("No id was given or in incorrect format");
-                        }
-                        break;
-                    case 'DELETE':
-                        // res.writeHead(200);
-                        // res.end(`We received ${methodType} type request`);
-                        if(parts.length == 3){
-                            deleteMethodHandler(parts[2],req, res);
-                        }
-                        else{
-                            res.writeHead(404);
-                            res.end("No id was given or in incorrect format");
-                        }
-                        break;
-                    default:
-                        res.writeHead(404);
-                        res.end(JSON.stringify({error: "Method not found :("}));
-                }
-                break
+                database = "static"
+                break;
+            case "performance":
+                database = "performance"
+                break;
+            case "active":
+                database = "active"
+                break;
             default:
                 res.writeHead(404);
                 res.end(JSON.stringify({error: "Resource not found :("}));
+        }
+        switch(methodType){
+            case 'GET':
+                if(parts.length == 2){ //send all the data
+                    getMethodHandler(null, req, res, database);
+                }
+                else if(parts.length == 3){
+                    getMethodHandler(parts[2], req, res,database);
+                }
+                else{
+                    res.writeHead(404);
+                    res.end(JSON.stringify({error: "Url not found :("}));
+                }
+                break;
+            case 'POST':
+                getRequestBodyAndGenerateResponse(req, res,po,database);
+                break;
+            case 'PUT':
+                // res.writeHead(200);
+                // res.end(`We received ${methodType} type request`);
+                if(parts.length == 3){
+                    putMethodHandler(parts[2],req, res,database);
+                }
+                else{
+                    res.writeHead(404);
+                    res.end("No id was given or in incorrect format");
+                }
+                break;
+            case 'DELETE':
+                // res.writeHead(200);
+                // res.end(`We received ${methodType} type request`);
+                if(parts.length == 3){
+                    deleteMethodHandler(parts[2],req, res,database);
+                }
+                else{
+                    res.writeHead(404);
+                    res.end("No id was given or in incorrect format");
+                }
+                break;
+            default:
+                res.writeHead(404);
+                res.end(JSON.stringify({error: "Method not found :("}));
         }        
 
     }catch (error){
@@ -302,8 +103,8 @@ const requestListener = function (req, res) {
 
 //----Get methods
 
-const getMethodHandler = (id, req, res) => {
-    getHandler(id).then(session => { //session must be a string
+const getMethodHandler = (id, req, res,database) => {
+    getHandler(id,database).then(session => { //session must be a string
         if(!session){
             res.writeHead(400);
             res.end(`The session with id ${id} is not found.`);
@@ -324,7 +125,7 @@ const getMethodHandler = (id, req, res) => {
         }   
     });
 }
-async function getHandler(id) {
+async function getHandler(id,database) {
     try{
         await client.connect();
         if(id != null){
@@ -346,7 +147,7 @@ async function getHandler(id) {
 
 //---Post methods
 
-async function postHandler(data){
+async function postHandler(data, database){
     try{
         await client.connect();
         const result = await client.db(database).collection("testing").insertOne(data);
@@ -357,10 +158,10 @@ async function postHandler(data){
         await client.close();
     }
 }
-async function po(res, body){
+async function po(res, body, database){
   try {
     let reqBody = body;
-    postHandler(reqBody);
+    postHandler(reqBody, database);
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.write(JSON.stringify(reqBody));
     res.end();
@@ -368,24 +169,24 @@ async function po(res, body){
         console.log(error.message);
     }
 }
-const getRequestBodyAndGenerateResponse = (req, res, callback) => {
+const getRequestBodyAndGenerateResponse = (req, res, callback,database) => {
   let body = '';
   req.on('data', chunk => {
     body += chunk.toString();
   });
   req.on('end', () => {
-    callback(res, JSON.parse(body));
+    callback(res, JSON.parse(body), database);
   });
 }
 //---- DELETE
 
-const deleteMethodHandler = (id, req, res) => {
-  const response = deleteSession(id);
+const deleteMethodHandler = (id, req, res, database) => {
+  const response = deleteSession(id,database);
   res.writeHead(200);
   res.end(`The employee with id ${id} is deleted.`);
 }
 
-async function deleteSession(id) {
+async function deleteSession(id, database) {
     try{
         await client.connect();
          const result = await client.db(database).collection("testing")
@@ -400,7 +201,7 @@ async function deleteSession(id) {
 }
 
 //---PUT
-const putMethodHandler = (id, req, res) => {
+const putMethodHandler = (id, req, res, database) => {
   let body = '';
   req.on('data', chunk => {
     body += chunk.toString();
@@ -409,11 +210,11 @@ const putMethodHandler = (id, req, res) => {
     body = JSON.parse(body);
   });
   console.log(body);
-  updateSession(id, body);
+  updateSession(id, body,database);
   res.writeHead(200);
   res.end(`The Employee object with id is ${id} replaced.`);
 }
-async function updateSession(id, data) {
+async function updateSession(id, data, database) {
     try{
         await client.connect();
         const result = await client.db(database).collection("testing")
